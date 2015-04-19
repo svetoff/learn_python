a3813dd8c9853cd69a9284dbdc2bc4e41f16626e
svetoff <vladimir.shvecov@gmail.com>
2015-04-19 16:46:26 +0300

    Добавлена задача 1_2_с

---
+++ task_1_2/task_1_2_c.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def find_letter(where, letter):
+    if letter in where:
+        where = where.split( )
+        lst = []
+        for word in where:
+            if word[0].lower() == letter.lower():
+                lst.append(word)
+                continue
+        return lst
+    else:
+        return ("Буквы - \"{0}\" нет в строке - \"{1}\"").format(letter, where)
+
+
+print find_letter("Bears are the best animals ever", 'b')
